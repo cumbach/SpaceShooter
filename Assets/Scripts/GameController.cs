484c31d5c1c3bb8ea3e3983f3a18663dba4502a2
Christopher Umbach <chrisumbach@gmail.com>
2016-06-05 16:10:54 -0700

    complete

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,63 +1,62 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour
+{
 	public GameObject[] hazards;
 	public Vector3 spawnValues;
 	public int hazardCount;
 	public float spawnWait;
 	public float startWait;
-	public float waveWait;
-	private int waveNumber;
+	public float waveWait; 
 
+	public GUIText scoreText;
 	public GUIText restartText;
 	public GUIText gameOverText;
-	public GUIText scoreText;
 
-	private int score;
 	private bool gameOver;
 	private bool restart;
+	private int score;
 
-	void Start () {
+	void Start ()
+	{
 		gameOver = false;
 		restart = false;
 		restartText.text = "";
 		gameOverText.text = "";
-		waveNumber = 0;
 		score = 0;
 		UpdateScore ();
 		StartCoroutine (SpawnWaves ());
 	}
 
-	void Update () {
-		if (restart) {
-			if (Input.GetKeyDown (KeyCode.R)) {
+	void Update ()
+	{
+		if (restart)
+		{
+			if (Input.GetKeyDown (KeyCode.R))
+			{
 				Application.LoadLevel (Application.loadedLevel);
 			}
 		}
 	}
 
-	IEnumerator SpawnWaves () {
+	IEnumerator SpawnWaves ()
+	{
 		yield return new WaitForSeconds (startWait);
-
-		while (true) {
-			waveNumber += 1;
-
-			for (int i = 0; i < hazardCount; i++) {
-				GameObject hazard = hazards[Random.Range(0, hazards.Length)];
+		while (true)
+		{
+			for (int i = 0; i < hazardCount; i++)
+			{
+				GameObject hazard = hazards[Random.Range (0, hazards.Length)];
 				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
 				Quaternion spawnRotation = Quaternion.identity;
-
 				Instantiate (hazard, spawnPosition, spawnRotation);
-
 				yield return new WaitForSeconds (spawnWait);
 			}
-
-			UpdateWave ();
-
 			yield return new WaitForSeconds (waveWait);
 
-			if (gameOver) {
+			if (gameOver)
+			{
 				restartText.text = "Press 'R' for Restart";
 				restart = true;
 				break;
@@ -65,22 +64,20 @@ public class GameController : MonoBehaviour {
 		}
 	}
 
-	void UpdateScore () {
-		scoreText.text = "Score: " + score;
-	}
-
-	public void AddScore (int newScoreValue) {
-		score += newScoreValue * waveNumber;
+	public void AddScore (int newScoreValue)
+	{
+		score += newScoreValue;
 		UpdateScore ();
 	}
 
-	void UpdateWave () {
-		hazardCount = (int)(Mathf.Floor ((float)hazardCount * 1.2f));
-		spawnWait = spawnWait * .95f;
+	void UpdateScore ()
+	{
+		scoreText.text = "Score: " + score;
 	}
 
-	public void GameOver () { 
+	public void GameOver ()
+	{
 		gameOverText.text = "Game Over!";
-		gameOver = true; 
+		gameOver = true;
 	}
 }
